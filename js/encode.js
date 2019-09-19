e608e52db81660b6602fada5d5893c20fc628fa6
pengyuan1990 <506756261@qq.com>
2019-09-19 12:59:51 +0800

    Create encode.js

---
+++ js/encode.js
@@ -0,0 +1,24 @@
+var __HMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    '`': '&#x60;'
+  };
+function _EncodeH(s){
+    if ((typeof s) !== "string"){
+        return "";
+    }
+    if (/(?:&|<|>|"|'|`)/.test(s)==false){
+        return s;
+    }
+    return s.replace(/(?:&|<|>|"|'|`)/g, function(match){
+        return __HMap[match];
+    });
+}
+
+function _EncodeUrlv(s){
+    // 为了让它转义出来的东西，可以直接放在 ' 包含的html field 里面。
+    return encodeURIComponent(s).replace("'","%27");
+}

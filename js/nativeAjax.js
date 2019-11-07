6677dfdcbfa5bbd31582c68f16f1c672fd500ebb
pengyuan1990 <506756261@qq.com>
2019-11-07 21:05:45 +0800

    Update nativeAjax.js

--- js/nativeAjax.js
+++ js/nativeAjax.js
@@ -1,13 +1,18 @@
-function ajaxSendByUrlQuery(url,key,value,success,error){
-    vaule = JSON.stringify(value);
-    function urlv(v){
+function urlv(v){
        return encodeURIComponent(v).replace("'","%27");
     }
+function urlAddParameter(url,key,value){
     if (url.indexOf("?")!==-1){
         url += "&"+urlv(key)+"="+urlv(value);
     }else{
         url += "?"+urlv(key)+"="+urlv(value);
     }
+    return url;
+}
+// value:json data
+function ajaxSendByUrlQuery(req){
+    vaule = JSON.stringify(req.value);
+    req.url = urlAddParameter(req.url,req.key,req.value);
     var oReq;
     if (window.XMLHttpRequest) {
         oReq = new XMLHttpRequest();
@@ -15,18 +20,25 @@ function ajaxSendByUrlQuery(url,key,value,success,error){
         oReq = new ActiveXObject("Microsoft.XMLHTTP");
     }
     oReq.onreadystatechange = function(){
-        if (oReq.readyState!==4){
-            return;
+        if (oReq.readyState===4){
+            if (oReq.status === 200) {
+                if (req.success){
+                    req.success(oReq);
+                }
+                if (req.complete){
+                    req.complete(oReq);
+                }
+            }else{// error status==0 timeout
+                if (req.complete){
+                    req.complete(oReq);
+                }
+                if (req.error){
+                    req.error(oReq);
+                }
+            }
         }
-        var resText = oReq.responseText;
-        if (oReq.status !== 200) { // error
-            error(resText);
-            return;
-        }
-        // success
-        success(resText);
     }
-    oReq.open("POST", url);
+    oReq.open("POST", req.url);
     oReq.timeout = 15000;
     oReq.send();
 }

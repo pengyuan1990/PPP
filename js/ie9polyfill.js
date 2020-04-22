096093ff73a10beade1e6542495cd02404496198
pengyuan1990 <506756261@qq.com>
2020-04-22 18:28:36 +0800

    Create ie9polyfill.js

---
+++ js/ie9polyfill.js
@@ -0,0 +1,38 @@
+if (!String.prototype.includes) {
+    String.prototype.includes = function (search, start) {
+        'use strict';
+        if (typeof start !== 'number') {
+            start = 0;
+        }
+
+        if (start + search.length > this.length) {
+            return false;
+        } else {
+            return this.indexOf(search, start) !== -1;
+        }
+    };
+}
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+        Element.prototype.matchesSelector ||
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.oMatchesSelector ||
+        Element.prototype.webkitMatchesSelector ||
+        function(s) {
+            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                i = matches.length;
+            while (--i >= 0 && matches.item(i) !== this) {}
+            return i > -1;
+        };
+}
+if (!Array.isArray){
+    Array.isArray = function(arg){
+      return Object.prototype.toString.call(arg) === '[object Array]';
+    };
+}
+if(!Number.isNaN){
+    Number.isNaN = function isNaN(input) {
+        return typeof input === 'number' && input !== input;
+    };
+}

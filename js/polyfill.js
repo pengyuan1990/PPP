c13ef8e54d2efb4c27d6500567861d8b11900bd2
pengyuan1990 <506756261@qq.com>
2021-06-24 10:55:06 +0800

    Update polyfill.js

--- js/polyfill.js
+++ js/polyfill.js
@@ -87,3 +87,19 @@ if (!window.cancelAnimationFrame) {
     clearTimeout(id);
   };
 }
+// Element remove action
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        this.parentNode && this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype].filter(Boolean));
